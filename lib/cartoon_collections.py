41a81802010e3e4f241f3fc2fe52a85f4f712800
Brian NJoroge <brian.njoroge1@student.moringaschool.com>
2023-08-22 18:45:53 +0300

    done with assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,26 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    for dwarf in dwarves:
+        print(dwarf)
+    
+def summon_captain_planet(planeteers):
+    capitalised_Planeteers = [planeteer.capitalize() + "!" for planeteer in planeteers]
+    return capitalised_Planeteers
 
-def summon_captain_planet():
-    pass
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+        else:
+            return False
+    
 
-def find_the_cheese():
-    pass
+def find_the_cheese(food_List):
+    for food in food_List:
+        if food == "cheddar":
+            return "cheddar"
+        elif food == "gouda":
+            return "gouda"
+        elif food == "camembert":
+            return "camembert"
+    return None
